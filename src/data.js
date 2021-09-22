0ae670d6acb6af5a072507f2749e860b24833787
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-09-22 12:01:41 +0200

    Write a function to iterate over the tasks array and populate an HTML list item element for each task

--- src/data.js
+++ src/data.js
@@ -1,18 +1,37 @@
 export const tasks = [
-    {
-      index: 0,
-      description: 'List structure.',
-      completed: false,
-    },
-    {
-      index: 1,
-      description: 'Interactive list.',
-      completed: false,
-    },
-    {
-      index: 2,
-      description: 'Add and remove items.',
-      completed: false,
-    },
-  ];
+  {
+    description: 'List structure.',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Interactive list.',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Add and remove items.',
+    completed: false,
+    index: 3,
+  },
+];
+
+const taskList = document.querySelector('.task-container .tasks');
+
+export const getTask = (task) => {
+    taskList.innerHTML += `<li class="book">
+    <span class="left">
+      <input id=${task.index} type="checkbox" />
+      <label for=${task.index}>${task.description}</label>
+    </span>
+    <span class="right">
+      <i class="fas fa-ellipsis-v"></i>
+    </span>
+  </li>`;
+  // return taskList;
+};
+
+
+
+
   
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-// import _ from 'lodash';
+import { getTask, tasks } from './data';
 
-//  function component() {
-//    const element = document.createElement('div');
+window.onload = () => {
+  tasks.forEach(task => {
+    getTask(task);
+  });
+}
 
-//   // Lodash, currently included via a script, is required for this line to work
-//   // Lodash, now imported by this script
-//    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-//    return element;
-//  }
 
-//  document.body.appendChild(component());
+
+
+
+
+
