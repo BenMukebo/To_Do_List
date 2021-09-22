8f44915eafe17eb4dcba3fe0b5d9fa8f77be1c08
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-09-22 10:56:48 +0200

    Setup local dev server to watch watch the source files

--- webpack.config.js
+++ webpack.config.js
@@ -2,31 +2,25 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
  module.exports = {
+  mode: 'development',
    entry: {
      index: './src/index.js',
    },
-   output: {
-     filename: 'main.js',
-     path: path.resolve(__dirname, 'dist'),
+   devtool: 'inline-source-map',
+   devServer: {
+     static: './dist',
    },
-   plugins: [
+
+  plugins: [
     new HtmlWebpackPlugin({
       title: 'Output Management',
       template: './src/index.html'
     }),
   ],
+
+   output: {
+     filename: 'main.js',
+     path: path.resolve(__dirname, 'dist'),
+   },
  };
 
-
-  // output: {
-  //   publicPath: '/',
-  // },
-
-  // plugins: [
-  //   new HtmlWebpackPlugin({
-  //     filename: './index.html',
-  //   }),
-  // ],
-  // devServer: {
-  //   contentBase: './dist',
-  // },
