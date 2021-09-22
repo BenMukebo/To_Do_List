d0f0bb1997051b6beda777bd533c5d138f272943
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-09-22 16:07:27 +0200

    Give Style to the whole project

--- src/data.js
+++ src/data.js
@@ -2,17 +2,17 @@ import { sortItems } from './utils';
 
 const tasks = [
   {
-    description: '1.List structure.',
+    description: 'List structure.',
     completed: false,
     index: 1,
   },
   {
-    description: '2.Interactive list.',
+    description: 'Interactive list.',
     completed: false,
     index: 2,
   },
   {
-    description: '3.Add and remove items.',
+    description: 'Add and remove items.',
     completed: false,
     index: 3,
   },
@@ -22,10 +22,10 @@ export const sortedTasks = sortItems(tasks);
 
 // console.log(sortedTasks);
 
-const taskList = document.querySelector('.task-container .tasks');
+const taskList = document.querySelector('.task-list');
 
 export const getTask = (task) => {
-    taskList.innerHTML += `<li class="book">
+    taskList.innerHTML += `<li class="book d-flex flex-center">
     <span class="left">
       <input id=${task.index} type="checkbox" />
       <label for=${task.index}>${task.description}</label>
