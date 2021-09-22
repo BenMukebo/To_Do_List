6bfc8321f19946adafe09ff28d1193a12f23d5c1
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-09-22 09:54:21 +0200

    Create a data.js file and set an array of objects

---
+++ src/data.js
@@ -0,0 +1,18 @@
+export const tasks = [
+    {
+      index: 0,
+      description: 'List structure.',
+      completed: false,
+    },
+    {
+      index: 1,
+      description: 'Interactive list.',
+      completed: false,
+    },
+    {
+      index: 2,
+      description: 'Add and remove items.',
+      completed: false,
+    },
+  ];
+  
