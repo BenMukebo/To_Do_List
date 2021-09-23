16f9bb1111ecc32423565cadd57a6b3c496ead43
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-09-23 11:32:21 +0200

    Add some methods to get and store the updated array of items in local storage

--- src/storage.js
+++ src/storage.js
@@ -1,4 +1,15 @@
 const storage = {
+  getItem() {
+    return JSON.parse(localStorage.getItem('tasks')) || [];
+  },
+
+  setItem(items) {
+    localStorage.setItem('tasks', JSON.stringify(items));
+  },
+
+  clear() {
+    localStorage.clear();
+  },
 };
 
 export default storage;
