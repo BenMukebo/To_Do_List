a208625a4118d6ac2fba686d726786de57e0f828
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-09-24 13:40:29 +0200

    Remove all hardcoded items in the tasks array

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import './style.css';
 import sortItems from './utils.js';
-import tasks from './data.js';
+// import tasks from './data.js';
 import completedTask from './controllers.js';
 import storage from './storage.js';
 
+const tasks = [];
 const taskList = document.querySelector('.task-list');
+
 const getTask = (task) => {
   taskList.innerHTML += `<li class="book d-flex flex-center">
     <span class="left">
@@ -17,6 +19,7 @@ const getTask = (task) => {
   </li>`;
 
   const books = document.querySelectorAll('.book');
+  // console.log(books)
 
   books.forEach((box) => {
     const checkBox = box.querySelector('.checkbox');
@@ -38,28 +41,27 @@ const getTask = (task) => {
 
     let change = false;
     icon.addEventListener('click', () => {
+      const indexBox = checkBox.id;
+      console.log('book Index:', indexBox);
       if (!change) {
         box.classList.add('active');
         icon.classList.remove('fas', 'fa-ellipsis-v');
         icon.classList.add('far', 'fa-trash-alt');
         change = true;
-      } else {
-        box.classList.remove('active');
-        icon.classList.remove('far', 'fa-trash-alt');
-        icon.classList.add('fas', 'fa-ellipsis-v');
-        change = false;
       }
     });
   });
 };
+
 let getTasks;
 
 (function setTaks() {
   if (!storage.getItem()) {
     storage.setItem(tasks);
     getTasks = storage.getItem();
+  } else {
+    getTasks = storage.getItem();
   }
-  getTasks = storage.getItem();
 }());
 
 window.onload = () => {
@@ -68,3 +70,11 @@ window.onload = () => {
     getTask(task);
   });
 };
+
+class Task {
+  constructor(description, index) {
+    this.description = description;
+    this.completed = false;
+    this.index = index;
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   devtool: 'inline-source-map',
   devServer: {
-    static: './dist',
+    static: './docs',
   },
 
   plugins: [
@@ -20,7 +20,7 @@ module.exports = {
 
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
   },
   module: {
     rules: [
